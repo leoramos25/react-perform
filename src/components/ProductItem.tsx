4af8511d3b6dcee4d88eb282a0aac3e5381f3ac7
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-06-24 08:54:17 -0300

    memo

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 type ProductItemProps = {
     product: {
         id: number;
@@ -6,10 +8,15 @@ type ProductItemProps = {
     }
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
     return (
         <div>
             {product.title} - <strong>{product.price}</strong>
         </div>
     )
 }
+
+// eslint-disable-next-line react/display-name
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+});
