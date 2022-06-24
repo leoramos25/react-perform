96073cdb9da68ee6e5fcc063ecfe395555fd8187
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2022-06-24 15:49:02 -0300

    data formatting

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -2,21 +2,17 @@ import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
 
 type SearchResultProps = {
+    totalPrice: number;
     results: Array<{
         id: number,
         price: number,
+        priceFormatted: string;
         title: string,
     }>
     onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults({ results, onAddToWishList }: SearchResultProps) {
-    const totalPrice = useMemo(() => {
-        return results.reduce((total, product) => {
-            return total + product.price;
-        }, 0);
-    }, [results]);
-
+export function SearchResults({ results, onAddToWishList, totalPrice }: SearchResultProps) {
     return (
         <div>
             <h2>{totalPrice}</h2>
